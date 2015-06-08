d1f7e2a72fbade23861d7630c732f93839ad1c11
clessmax <vuongvantruong1987@gmail.com>
2015-06-08 17:17:09 +0700

    add

--- QrcodeOrBarCode/src/vnp/com/qrcode/main/MainActivity.java
+++ QrcodeOrBarCode/src/vnp/com/qrcode/main/MainActivity.java
@@ -1,5 +1,6 @@
 package vnp.com.qrcode.main;
 
+import vnp.com.qrcode.R;
 import android.os.Bundle;
 
 import com.vnp.core.activity.BaseFragmentActivity;
@@ -8,5 +9,6 @@ public class MainActivity extends BaseFragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_main);
 	}
 }
